01f7d8a7e7478edbf694887b3eedd9502216d46a
AlMax <alihaider.maqsood@gmail.com>
2020-09-03 15:42:02 +0200

    Risolti buchi di logica, implementate funzioni aggiuntive

--- RinominaPdf/RinominarePdfDaInput.py
+++ RinominaPdf/RinominarePdfDaInput.py
@@ -1,25 +1,41 @@
-import ctypes
-import PyPDF2
-import shlex
 import os
-from progressbar import ProgressBar
-from datetime import date
-
+import ctypes
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+import tkinter as tk
+from tkinter import simpledialog
+import sys
 
-pbar = ProgressBar()
-df = pd.read_excel('rinomina.xlsx')
+ROOT = tk.Tk()
+ROOT.withdraw()
+nomeExcel = simpledialog.askstring(title="Rinominatore PDF tramite Excel",
+                                  prompt="Inserire il nome del file xlsx senza l'estensione.\nEsempio: se il file si chiama 'test.xlsx', basterà inserire 'test'")
 
-#print("Column headings:")
-#print(df.columns)
-#print("\n\nStampa colonna1")
-#print(df['old'])
+def Mbox(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
-for i,j in df['old'].iteritems():
-    old = df['old'][i]
-    new = df['nuovo'][i]
-    old_name = old
-    new_name = new
-    os.rename(old_name, new_name)
+successi = "I seguenti file sono stati rinominati con successo:\n"
+errori = "I seguenti file non sono stati trovati:\n"
+
+try:
+    #Leggo l'excel
+    excelReader = pd.read_excel('%s.xlsx' %nomeExcel)
+except Exception as erroreLettura:
+    Mbox("Rinominatore PDF tramite Excel By ALMAX (GitHub)","Non esiste alcun excel denominato " + nomeExcel + ".xlsx\nIl programma verrà chiuso.", 1)
+    sys.exit();
+
+print("Adesso inizio ad eseguire le elaborazioni, di seguito eventuali errori.\n")
+for indice,valore in excelReader['old'].iteritems():
+#In questo loop analizziamo ogni singolo elemento della colonna 'old' dell'excel; l'oggetto excelReader è una Serie
+    try:
+        old_name = excelReader['old'][indice] + ".pdf"
+        new_name = excelReader['new'][indice] + ".pdf"
+        os.rename(old_name, new_name)
+        successi += old_name + "-->" + new_name + "\n"
+    except Exception as erroreBloccante:
+        errori += old_name + "\n"
+        print(erroreBloccante)
+        #erroreBloccante contiene il log dell'errore
+
+Mbox("Rinominatore PDF tramite Excel By ALMAX (GitHub)",successi + "\n" + errori, 1)
