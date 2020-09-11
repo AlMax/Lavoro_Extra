d175bb310d6a7f0dd0d792510c9dede6b3ed43bf
AlMax <alihaider.maqsood@gmail.com>
2020-09-11 09:16:44 +0200

    sistemati bug minori

--- Dividi_Cerca_PdfCF/Dividi_Cerca_PdfPerCodiceFiscale.py
+++ Dividi_Cerca_PdfCF/Dividi_Cerca_PdfPerCodiceFiscale.py
@@ -6,54 +6,66 @@ from progressbar import ProgressBar
 from datetime import date
 import sys
 import pandas as pd
+from tkinter import simpledialog
+import tkinter as tk
 
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
 try:
+
+    ROOT = tk.Tk()
+    ROOT.withdraw()
+
     # Oggetto PDF
-    pdfFileObj = open('cedolini.pdf', 'rb')
+    nomePDF = simpledialog.askstring(title="Dividi e Cerca PDF tramite Excel",
+                                      prompt="Inserire il nome del file PDF senza l'estensione.\nEsempio: se il file si chiama 'test.pdf', basterà inserire 'test'")
+    pdfFileObj = open(nomePDF+".pdf", 'rb')
 
     # Readaer del PDF
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     numPagine = pdfReader.numPages
     divisioniPagine = ""
     today = date.today()
-    directorySalvataggio = "Cedolini_Divisi " + today.strftime("(%d-%m-%Y)")
+    directoryDivisi = "Cedolini_Divisi " + today.strftime("(%d-%m-%Y)")
+    directoryCercati = "Cedolini_Trovati " + today.strftime("(%d-%m-%Y)")
     pbar = ProgressBar()
     colonnaExcel = 'cercare'
     pdf_da_trovare = []
-    nomeExcel = "CF"
+    nomeExcel = simpledialog.askstring(title="Dividi e Cerca PDF tramite Excel",
+                                      prompt="Inserire il nome del file xlsx senza l'estensione.\nEsempio: se il file si chiama 'test.xlsx', basterà inserire 'test'")
 
     try:
         #Leggo l'excel
         excelReader = pd.read_excel('%s.xlsx' %nomeExcel)
     except Exception as erroreLettura:
-        Mbox("Cerca PDF tramite Excel By ALMAX (GitHub)","Non esiste alcun excel denominato " + nomeExcel + ".xlsx\nIl programma verrà chiuso.", 1)
+        Mbox("Dividi e Cerca PDF tramite Excel By ALMAX (GitHub)","Non esiste alcun excel denominato " + nomeExcel + ".xlsx\nIl programma cercherà comunque di dividere il singolo PDF.", 1)
         print(erroreLettura)
-        sys.exit();
+
 
     try:
-        os.mkdir(directorySalvataggio)
+        os.mkdir(directoryDivisi)
+        os.mkdir(directoryCercati)
     except OSError:
         errore =  "Errore nel creare la cartella, probabilmente esiste già.\nSi raccomanda di far partire un programma in una cartella vuota.\nIl programma cercherà comunque di generare i PDF ma potrebbero esserci delle anomalie e mancare dei PDF.\n\n"
     else:
         errore = "Tutto è andato a Buon fine.\n\n"
 
+    try:
+        for indice,valore in excelReader[colonnaExcel].iteritems():
+        #In questo loop analizziamo ogni singolo elemento della colonna dell'excel; l'oggetto excelReader è una Serie
+            try:
+                pdf_name = excelReader[colonnaExcel][indice]
+                if(pdf_name[-4:] != ".pdf"):
+                    pdf_name += ".pdf"
 
-    for indice,valore in excelReader[colonnaExcel].iteritems():
-    #In questo loop analizziamo ogni singolo elemento della colonna dell'excel; l'oggetto excelReader è una Serie
-        try:
-            pdf_name = excelReader[colonnaExcel][indice]
-            if(pdf_name[-4:] != ".pdf"):
-                pdf_name += ".pdf"
+                pdf_da_trovare.append(pdf_name)
+            except Exception as erroreBloccante:
+                print(erroreBloccante)
+                #erroreBloccante contiene il log dell'errore
+    except Exception:
+        print("no")
 
-            pdf_da_trovare.append(pdf_name)
-        except Exception as erroreBloccante:
-            print(erroreBloccante)
-            #erroreBloccante contiene il log dell'errore
-
-    print(pdf_da_trovare)
     # Ciclo per analizzare ogni singola pagina e dividerla
     for i in pbar(range(numPagine)):
 
@@ -64,9 +76,9 @@ try:
         try:
             txtExtract = shlex.split(pageTxt, posix=False)
         except Exception as errorShlex:
-            print(str(errorShlex) + str(i+1))
+            print("Errore di shlex: " + str(errorShlex) + " alla pagina " + str(i+1))
 
-
+        parola = "errore"
         # Estrapolo il codice fiscale in base alla sua composizione
         for parola in txtExtract:
             if len(parola) == 16:
@@ -74,26 +86,23 @@ try:
                     codiceFiscale = parola
                     codiceFiscaleStampa = codiceFiscale + ".pdf"
 
-        # Estrapolo il codice fiscale utilizzando la strina la posizione previdenziale=A95 e creo una stringa che riporti le operazioni fatte
-    #    indexCF = txtExtract.index("A95") - 1
-    #    codiceFiscale = txtExtract[indexCF]
-    #    divisioniPagine += "Codice Fiscale della pagina " + str(i+1) + " --> " + codiceFiscale + "\n"
-
         # Divido la singola pagina dal PDF
+        divisorePDF = PyPDF2.PdfFileWriter()
+        divisorePDF.addPage(pdfReader.getPage(i))
         if codiceFiscaleStampa in pdf_da_trovare:
-            divisorePDF = PyPDF2.PdfFileWriter()
-            divisorePDF.addPage(pdfReader.getPage(i))
-            with open(directorySalvataggio + "/" + codiceFiscaleStampa, "wb") as pdfDiviso:
+            with open(directoryCercati + "/" + codiceFiscaleStampa, "wb") as pdfDiviso:
                 divisorePDF.write(pdfDiviso)
-            divisioniPagine += "Codice Fiscale della pagina " + str(i+1) + " --> " + codiceFiscaleStampa + "\n"
+        with open(directoryDivisi + "/" + codiceFiscaleStampa, "wb") as pdfDiviso:
+            divisorePDF.write(pdfDiviso)
+        divisioniPagine += "Codice Fiscale della pagina " + str(i+1) + " --> " + codiceFiscaleStampa + "\n"
 
     # Mostro una Form con le divisioni delle pagine fatte
     numPagineOut = errore + "Numero di pagine divise: " + str(numPagine) + "\n"
     divisioniPagine += "\n\nOperazione conclusa.\nGrazie mille e Buon lavoro"
-    Mbox("Divisore PDF tramite Codice Fiscale By ALMAX (GitHub)",numPagineOut + divisioniPagine, 1)
+    Mbox("Dividi e Cerca PDF tramite Codice Fiscale By ALMAX (GitHub)",numPagineOut + divisioniPagine, 1)
 
     # Chiudo l'oggetto file
     pdfFileObj.close()
 except Exception as erroreBloccante:
-    messaggioErroreBloccante = "Il programma non riesce a partire a causa di un errore.\nSi ricorda che è obbligatorio rinominare il pdf da dividere in 'cedolini.pdf'\n\nIn caso non funzioni ancora, contattarmi alla mail ali.haider.maqsood@maw.it\n\nL'errore è:\n" + str(erroreBloccante)
-    Mbox("Divisore PDF tramite Codice Fiscale By ALMAX (GitHub)", messaggioErroreBloccante, 1)
+    messaggioErroreBloccante = "Il programma non riesce a partire a causa di un errore.\n\nIn caso non funzioni ancora, contattarmi alla mail ali.haider.maqsood@maw.it\n\nL'errore è:\n" + str(erroreBloccante)
+    Mbox("Dividi e Cerca PDF tramite Codice Fiscale By ALMAX (GitHub)", messaggioErroreBloccante, 1)
