432abc5eb1b710e3ffd2b1362b85885cdd321293
AlMax <alihaider.maqsood@gmail.com>
2020-09-29 17:59:27 +0200

    programma completo
    la GUI è ottimale, mgiliorabile ma funziona correttamente. Gli algoritmi interni sembra tutti ok

---
+++ Excel_XML_Converter/frames.py
@@ -0,0 +1,84 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import ttk
+import sys
+import os
+from tkinter.filedialog import askopenfilename
+import funzioni as functions
+
+def RichiediFile(nome_programma):
+
+    def caricaFile(button, fileType, valori_lettura):
+        file = askopenfilename(filetypes = fileType)
+        if file != "":
+            valori_lettura.append(file)
+            button['text'] = os.path.basename(file)
+            button['state'] = "disabled"
+            return valori_lettura
+        button['text'] = "Errore! Riprovare"
+
+    def ripristina(all_buttons, all_texts, valori_lettura):
+        indice = 0
+        for button in all_buttons:
+            button['text'] = all_texts[indice]
+            button['state'] = "enabled"
+            indice += 1
+        valori_lettura.clear()
+
+    def conferma(bottoni_da_disabilitare, all_buttons, all_texts, valori_lettura):
+        valori_lettura.append(field_txt_istanze.get())
+        valori_lettura.append(field_txt_lavoratori.get())
+        if len(valori_lettura) != 3:
+            ripristina(all_buttons, all_texts, valori_lettura)
+            return
+        for button in bottoni_da_disabilitare:
+            button['state'] = "disabled"
+        functions.logOperazioni("Confermato")
+        root.quit()
+
+    try:
+        buttons = []
+        texts = []
+        valori_lettura = []
+
+        #txt_pdf = "Selezione il file PDF"
+        txt_xls = "Selezione il file EXCEL"
+        txt_label = "Selezionare l'Excel da leggere\n\n\nInserire il nome del foglio con i dati delle Istanze\n\n\nInserire il nome del foglio con i dati dei Lavoratori\n\n\nBarra del progresso.\nOgni completamento significa il completamento \ndi un'istanza Cliente con relativi Lavoratori."
+        texts.extend([txt_xls, txt_label])
+
+        root = Tk()
+        root.title(nome_programma)
+        root.geometry('450x270')
+        root.resizable(0, 0)
+
+        valore = StringVar()
+        valore1 = StringVar()
+
+        label = Label(root, text= txt_label).pack(side= LEFT,anchor = NW, pady = 12, padx = 15)
+
+        #btn_pdf = Button(root, text = txt_pdf, command = lambda:caricaFile(btn_pdf, [(txt_pdf, "*.pdf")], valori_lettura))
+        btn_xls = Button(root, text = txt_xls, command = lambda:caricaFile(btn_xls, [(txt_xls, "*.xls"), (txt_xls, "*.xlsx")], valori_lettura))
+        field_txt_istanze = Entry(root, textvariable=valore, width = 30)
+        valore.set("DATI ISTANZE")
+        field_txt_lavoratori = Entry(root, textvariable=valore1, width = 30)
+        valore1.set("DATI LAVORATORI")
+        buttons.extend([btn_xls])
+        progressBar = ttk.Progressbar(root, orient="horizontal", length=286,mode="determinate")
+                
+        btn_modifica = Button(root, text ='RIPRISTINA', command = lambda:ripristina(buttons, texts, valori_lettura))
+        btn_conferma = Button(root, text ='CONFERMA', command = lambda:conferma([field_txt_istanze, field_txt_lavoratori, btn_modifica, btn_conferma],buttons, texts, valori_lettura))
+
+        #btn_pdf.pack(side = TOP, anchor = NW, pady = 10, padx = 10)
+        btn_xls.pack(side = TOP, anchor = NW, pady = 10, padx = 10)
+        field_txt_istanze.pack(anchor = NW, pady = 10, padx = 10)
+        field_txt_lavoratori .pack(anchor = NW, pady = 10, padx = 10)
+        progressBar.pack(anchor = NW, pady = 34, padx = 10)
+
+        btn_modifica.place(relx=0.3, rely=0.85, anchor=CENTER)
+        btn_conferma.place(relx=0.7, rely=0.85, anchor=CENTER)
+    
+
+        root.mainloop()
+        return valori_lettura, progressBar
+    except Exception as erroreFrame:
+        functions.logOperazioni(str(erroreFrame))
--- Excel_XML_Converter/xls_xml_converter.py
+++ Excel_XML_Converter/xls_xml_converter.py
@@ -6,163 +6,209 @@ import re
 from lxml import etree
 from io import StringIO
 import sys
+import frames as frame
+from datetime import date
+import datetime
+from tkinter import *
+from tkinter import ttk
+import time
 
-documento = minidom.Document()
-xml = documento.createElement('tis')
-documento.appendChild(xml)
 
-attributi_istanza = []
-attributi_istanza.extend(["identificativoPratica", "denominazioneAPL", "ragioneSocialeUtilizzatore", "partitaIvaUtilizzatore", "matricolaInpsUtilizzatore", "codiceAtecoUtilizzatore", "sedeUnitaProduttiva", "mensilitaAggiuntive", "settoreRiferimento", "autocertificazioneSettimane", "note"])
+try: 
+    today = date.today().strftime("%d-%m-%Y")
+    now = datetime.datetime.now().strftime("%H.%M.%S")
 
-dati_tutti_lavoratori = []
-dati_tutti_lavoratori.extend(["annoRiferimento", "meseRiferimento", "codiceFiscale", "cognome", "nome", "tipologiaContratto", "sgraviAliquotaContributivaPrevidenziale", "retribuzioneMensileLorda", "retribuzioneTisRiconosciuta", "contribuzioneTisRiconosciuta", "quotaRateiMensilitaAggiuntive", "quotaRateiRolPermessiFerie", "totaleOreTisRiconosciute", "lavoratoreAlleDipendenze25Marzo"])
+    functions.logOperazioni("")
 
-tutti_lavoratori = [[]]
-tutti_clienti = [[]]
-array = []
-lista = []
-identificativi_lavoratori = []
+    if os.stat("Log.txt").st_size == 0:
+        functions.logOperazioni("Scorrere in basso per avere i log piu' recenti.\nI Log vengono registrati ogni volta che viene lanciato il programma.")
+    functions.logOperazioni("\n\nI seguenti Log fanno riferimento al giorno " + today + " alle ore " + now + "\n")
 
-#Lettura Excel
-nomeExcel = "FORMAT RENDICONTAZIONE.xlsx"
+    documento = minidom.Document()
+    xml = documento.createElement('tis')
+    documento.appendChild(xml)
 
-functions.leggiExcel_colonna(functions.leggiExcel(nomeExcel, "DATI LAVORATORI"), "identificativoPratica", identificativi_lavoratori)
+    attributi_istanza = []
+    attributi_istanza.extend(["identificativoPratica", "denominazioneAPL", "ragioneSocialeUtilizzatore", "partitaIvaUtilizzatore", "matricolaInpsUtilizzatore", "codiceAtecoUtilizzatore", "sedeUnitaProduttiva", "mensilitaAggiuntive", "settoreRiferimento", "autocertificazioneSettimane", "note"])
 
-indice = 0
-for dato_lavoratori in dati_tutti_lavoratori:
-    colonnaExcel = dato_lavoratori
-    functions.leggiExcel_colonna(functions.leggiExcel(nomeExcel, "DATI LAVORATORI"), colonnaExcel, array)
-    if indice == 0:
-        tutti_lavoratori[0] = array.copy()
-    else:
-        lista = array.copy()
-        tutti_lavoratori.append(lista)
-    array.clear()
-    indice += 1
+    dati_tutti_lavoratori = []
+    dati_tutti_lavoratori.extend(["annoRiferimento", "meseRiferimento", "codiceFiscale", "cognome", "nome", "tipologiaContratto", "sgraviAliquotaContributivaPrevidenziale", "retribuzioneMensileLorda", "retribuzioneTisRiconosciuta", "contribuzioneTisRiconosciuta", "quotaRateiMensilitaAggiuntive", "quotaRateiRolPermessiFerie", "totaleOreTisRiconosciute", "lavoratoreAlleDipendenze25Marzo"])
 
-indice = 0
-for attributo in attributi_istanza:
-    colonnaExcel = attributo
-    functions.leggiExcel_colonna(functions.leggiExcel(nomeExcel, "DATI ISTANZE"), colonnaExcel, array)
-    if indice == 0:
-        tutti_clienti[0] = array.copy()
-    else:
-        lista = array.copy()
-        tutti_clienti.append(lista)
-    array.clear()
-    indice += 1
+    tutti_lavoratori = [[]]
+    tutti_clienti = [[]]
+    array = []
+    lista = []
+    identificativi_lavoratori = []
 
-indice_dato_cliente = 0
-for cliente in range(len(tutti_clienti[0])):
-    padre = documento.createElement('istanza')
-    xml.appendChild(padre)
+    nomeProgramma = "Convertitore XML By ALMAX (GitHub)"
+    frameReturn = frame.RichiediFile(nomeProgramma)
 
-    indice_array_cliente = 0
+    nomi_file = frameReturn[0]
+    nomeExcel = [nome for nome in nomi_file if ".xls" in nome][0]
+
+    progressBar = frameReturn[1]
+
+
+    functions.leggiExcel_colonna(functions.leggiExcel(nomeExcel, nomi_file[2]), "identificativoPratica", identificativi_lavoratori)
+
+
+    indice = 0
+    for dato_lavoratori in dati_tutti_lavoratori:
+        colonnaExcel = dato_lavoratori
+        functions.leggiExcel_colonna(functions.leggiExcel(nomeExcel, nomi_file[2]), colonnaExcel, array)
+        if indice == 0:
+            tutti_lavoratori[0] = array.copy()
+        else:
+            lista = array.copy()
+            tutti_lavoratori.append(lista)
+        array.clear()
+        indice += 1
+
+    progressBar['maximum'] = len(tutti_lavoratori[2])
+
+    indice = 0
     for attributo in attributi_istanza:
-        figlio = documento.createElement(attributo)
-        dato_cliente = tutti_clienti[indice_array_cliente]
-        figlio.appendChild(documento.createTextNode(dato_cliente[indice_dato_cliente]))
-        padre.appendChild(figlio)
-        indice_array_cliente += 1
+        colonnaExcel = attributo
+        functions.leggiExcel_colonna(functions.leggiExcel(nomeExcel, nomi_file[1]), colonnaExcel, array)
+        if indice == 0:
+            tutti_clienti[0] = array.copy()
+        else:
+            lista = array.copy()
+            tutti_clienti.append(lista)
+        array.clear()
+        indice += 1
+
+    indice_dato_cliente = 0
+    for cliente in range(len(tutti_clienti[0])):
+        indice_array_cliente = 0
+
+        functions.logOperazioni("Inizio estrapolazione del cliente " + str(tutti_clienti[indice_array_cliente]) + "\n")
+        padre = documento.createElement('istanza')
+        xml.appendChild(padre)
     
-
-    figlio = documento.createElement('lavoratori')
-    padre.appendChild(figlio)
-
-    indice_dato_lavoratore = 0
-    for lavoratore in range(len(tutti_lavoratori[2])):
+        for attributo in attributi_istanza:
+            figlio = documento.createElement(attributo)
+            dato_cliente = tutti_clienti[indice_array_cliente]
+            if dato_cliente[indice_dato_cliente] == 'nan':
+                figlio.appendChild(documento.createTextNode(""))
+            else:
+                figlio.appendChild(documento.createTextNode(dato_cliente[indice_dato_cliente]))
+            padre.appendChild(figlio)
+            indice_array_cliente += 1
         
-        identificativoPratica = tutti_clienti[0]
-        if identificativi_lavoratori[lavoratore] == identificativoPratica[indice_dato_cliente]:
-            nipote = documento.createElement('lavoratore')
-            figlio.appendChild(nipote)
 
-        indice_array_lavoratore = 0
-        for dati_lavoratore in dati_tutti_lavoratori:
-            
+        figlio = documento.createElement('lavoratori')
+        padre.appendChild(figlio)
+
+        indice_dato_lavoratore = 0
+        
+        for lavoratore in range(len(tutti_lavoratori[2])):
+
+            progressBar["value"] = indice_dato_lavoratore
+            time.sleep(0.05)
+            progressBar.update()
+
+            functions.logOperazioni("Estrapolato " + str(indice_dato_lavoratore+1) + " lavoratore su " + str(len(tutti_lavoratori[2])) + " lavoratori.\n")
+            identificativoPratica = tutti_clienti[0]
             if identificativi_lavoratori[lavoratore] == identificativoPratica[indice_dato_cliente]:
-                pro_nipote = documento.createElement(dati_lavoratore)
-                dato_lavoratore = tutti_lavoratori[indice_array_lavoratore]
-                pro_nipote.appendChild(documento.createTextNode(dato_lavoratore[indice_dato_lavoratore]))
-                nipote.appendChild(pro_nipote)
-            
-            indice_array_lavoratore += 1
-        indice_dato_lavoratore += 1
-    indice_dato_cliente += 1
+                nipote = documento.createElement('lavoratore')
+                figlio.appendChild(nipote)
 
-
-xml_str = documento.toprettyxml(indent="\t")
-
-
-save_path_file = "test.xml"
-
-with open(save_path_file, "w") as f:
-    f.write(xml_str)
-
-#tree = etree.parse("test.xml")
-#string = etree.tostring(tree.getroot(), pretty_print = True, xml_declaration = True, standalone = True, encoding = "UTF-8")
-#with open("test.xml", "wb") as f:
-#    f.write(string)
+            indice_array_lavoratore = 0
+            for dati_lavoratore in dati_tutti_lavoratori:
+                
+                if identificativi_lavoratori[lavoratore] == identificativoPratica[indice_dato_cliente]:
+                    pro_nipote = documento.createElement(dati_lavoratore)
+                    dato_lavoratore = tutti_lavoratori[indice_array_lavoratore]
+                    pro_nipote.appendChild(documento.createTextNode(dato_lavoratore[indice_dato_lavoratore]))
+                    nipote.appendChild(pro_nipote)
+                
+                indice_array_lavoratore += 1
+            indice_dato_lavoratore += 1
+        
+        functions.logOperazioni("Concludo estrpolazione del cliente " + str(indice_dato_cliente+1) + "\n")
+    
+        indice_dato_cliente += 1
 
 
 
-filename_xml = "test.xml"
-filename_xsd = "validation.xsd"
-
-# open and read schema file
-with open(filename_xsd, 'r') as schema_file:
-    schema_to_check = schema_file.read()
-
-# open and read xml file
-with open(filename_xml, 'r') as xml_file:
-    xml_to_check = xml_file.read()
-
-xmlschema_doc = etree.parse(StringIO(schema_to_check))
-xmlschema = etree.XMLSchema(xmlschema_doc)
+    xml_str = documento.toprettyxml(indent="\t")
 
 
-try:
-    doc = etree.parse(StringIO(xml_to_check))
-    print('XML well formed, syntax ok.')
+    save_path_file = "test.xml"
 
-# check for file IO error
-except IOError:
-    print('Invalid File')
-
-# check for XML syntax errors
-except etree.XMLSyntaxError as err:
-    print('XML Syntax Error, see error_syntax.log')
-    with open('error_syntax.log', 'w') as error_log_file:
-        error_log_file.write(str(err.error_log))
-    quit()
-
-except Exception as errore:
-    print(errore)
-    quit()
+    with open(save_path_file, "w") as f:
+        f.write(xml_str)
 
 
-# validate against schema
-try:
-    xmlschema.assertValid(doc)
-    print('XML valid, schema validation ok.')
+    filename_xml = "test.xml"
+    filename_xsd = "validation.xsd"
 
-except etree.DocumentInvalid as err:
-    print('Schema validation error, see error_schema.log')
-    with open('error_schema.log', 'w') as error_log_file:
-        error_log_file.write(str(err.error_log))
-    quit()
+    # open and read schema file
+    with open(filename_xsd, 'r') as schema_file:
+        schema_to_check = schema_file.read()
 
-except:
-    print('Unknown error, exiting.')
-    quit()
+    # open and read xml file
+    with open(filename_xml, 'r') as xml_file:
+        xml_to_check = xml_file.read()
 
+    xmlschema_doc = etree.parse(StringIO(schema_to_check))
+    xmlschema = etree.XMLSchema(xmlschema_doc)
 
 
+    try:
+        doc = etree.parse(StringIO(xml_to_check))
+        print('XML well formed, syntax ok.')
+
+    # check for file IO error
+    except IOError:
+        print('Invalid File')
+
+    # check for XML syntax errors
+    except etree.XMLSyntaxError as err:
+        print('XML Syntax Error, see error_syntax.log')
+        with open('error_syntax.log', 'w') as error_log_file:
+            error_log_file.write(str(err.error_log))
+        quit()
+
+    except Exception as errore:
+        print(errore)
+        quit()
 
 
-xml_file = etree.parse("test.xml")
-xml_validator = etree.XMLSchema(file="validation.xsd")
+    # validate against schema
+    try:
+        xmlschema.assertValid(doc)
+        print('XML valid, schema validation ok.')
 
-is_valid = xml_validator.validate(xml_file)
+    except etree.DocumentInvalid as err:
+        print('Schema validation error, see error_schema.log')
+        with open('error_schema.log', 'w') as error_log_file:
+            error_log_file.write(str(err.error_log))
+        quit()
 
-print("L'XML rispetta la struttura definita nell' XSD? " + str(is_valid))
+    except:
+        print('Unknown error, exiting.')
+        quit()
+
+
+
+
+
+    xml_file = etree.parse("test.xml")
+    xml_validator = etree.XMLSchema(file="validation.xsd")
+
+    is_valid = xml_validator.validate(xml_file)
+
+    print("L'XML rispetta la struttura definita nell' XSD? " + str(is_valid))
+
+
+    tree = etree.parse("test.xml")
+    string = etree.tostring(tree.getroot(), pretty_print = False, xml_declaration = True, standalone = True, encoding = "UTF-8")
+    with open("test.xml", "wb") as f:
+        f.write(string)
+
+    
+   
+except Exception as erroreGenerale:
+    print(str(erroreGenerale)) 
