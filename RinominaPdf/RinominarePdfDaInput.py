fa97f94445b8279039f0251d69f2a84a8a3823ef
AlMax <alihaider.maqsood@gmail.com>
2020-10-06 12:57:33 +0200

    coorezioni minori

--- RinominaPdf/RinominarePdfDaInput.py
+++ RinominaPdf/RinominarePdfDaInput.py
@@ -15,8 +15,15 @@ nomeExcel = simpledialog.askstring(title="Rinominatore PDF tramite Excel",
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
+def logOperazioni(log):
+    """Scrittura dei log su apposito file"""
+    fileLog = open("Log.txt", "a")
+    fileLog.write(log)
+    fileLog.close()
+
 successi = "I seguenti file sono stati rinominati con successo:\n"
 errori = "I seguenti file non sono stati trovati:\n"
+old_name = ""
 
 try:
     #Leggo l'excel
@@ -25,17 +32,17 @@ except Exception as erroreLettura:
     Mbox("Rinominatore PDF tramite Excel By ALMAX (GitHub)","Non esiste alcun excel denominato " + nomeExcel + ".xlsx\nIl programma verrà chiuso.", 1)
     sys.exit();
 
-print("Adesso inizio ad eseguire le elaborazioni, di seguito eventuali errori.\n")
+#print("Adesso inizio ad eseguire le elaborazioni, di seguito eventuali errori.\n")
 for indice,valore in excelReader['old'].iteritems():
 #In questo loop analizziamo ogni singolo elemento della colonna 'old' dell'excel; l'oggetto excelReader è una Serie
     try:
-        old_name = excelReader['old'][indice] + ".pdf"
-        new_name = excelReader['new'][indice] + ".pdf"
+        old_name = str(excelReader['old'][indice]) + ".pdf"
+        new_name = str(excelReader['new'][indice]) + ".pdf"
         os.rename(old_name, new_name)
         successi += old_name + "-->" + new_name + "\n"
     except Exception as erroreBloccante:
         errori += old_name + "\n"
-        print(erroreBloccante)
         #erroreBloccante contiene il log dell'errore
 
-Mbox("Rinominatore PDF tramite Excel By ALMAX (GitHub)",successi + "\n" + errori, 1)
+logOperazioni(successi + "\n" + errori)
+Mbox("Rinominatore PDF tramite Excel By ALMAX (GitHub)","Consultare il file Log.txt", 1)
