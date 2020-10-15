0aefd6c5f45def5583ca559e3677d87c25a5742a
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2020-10-15 21:09:14 +0200

    Quasi completo
    Il programma è funzionante secondo l'obbiettivo richiesto. Mancano alcuni accorgimenti per la release finale

--- Modifica_campi_xml/modifica_campi_xml.py
+++ Modifica_campi_xml/modifica_campi_xml.py
@@ -5,14 +5,17 @@ from xml.dom import minidom
 import xml.etree.ElementTree as ET
 import xml_structure as xmlManipulation
 import frames as frame
-
+import os
 
 ET.register_namespace("", "http://servizi.lavoro.gov.it/unisomm")
 namespace = "{http://servizi.lavoro.gov.it/unisomm}"
 returnFrame = frame.RichiediFile("Programmino Laura")
+campi = returnFrame[1]
+tutte_coordinate = returnFrame[2]
+nome_zip = returnFrame[0]
+coordinate = []
 
-
-with ZipFile('/Users/Ali Haider Maqsood/Documents/GitHub/MAW/Modifica_campi_xml/compressa.zip', 'r') as zipObj:
+with ZipFile(nome_zip, 'r') as zipObj:
     listOfiles = zipObj.namelist()
     for file in listOfiles:
         tree = ET.parse(zipObj.open(file))
@@ -22,11 +25,21 @@ with ZipFile('/Users/Ali Haider Maqsood/Documents/GitHub/MAW/Modifica_campi_xml/
             xmlns_setting.set("xmlns:xsd", "http://www.w3.org/2001/XMLSchema")
             xmlns_setting.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
 
-        for campi_modifica in range(returnFrame[1]):
-            coordinate = returnFrame[2]
-            xmlManipulation.modificaCampo(root, namespace, coordinate)
+        for campi_modifica in range(campi):
+            i = campi_modifica
+            coordinate.clear()
+            for coordinata in range(int(len(tutte_coordinate)/campi)-1):
 
+                coordinate.append(tutte_coordinate[i])
+                i += campi
 
-        tree.write("newitems" + str(file),encoding="utf-8", xml_declaration=True)
+            xmlManipulation.modificaCampo(root, namespace, coordinate, tutte_coordinate[i])
+
+os.remove(nome_zip)
+zipNuovo = ZipFile(nome_zip, 'w')
+for file in listOfiles:
+    tree.write(str(file),encoding="utf-8", xml_declaration=True)
+    zipNuovo.write(file)
+    os.remove(file)
 
 
